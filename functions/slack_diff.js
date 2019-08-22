69c0901354f273cfe3c0bf83f4f236e3260ef2e0
ckazu <401343+ckazu@users.noreply.github.com>
2019-08-22 19:44:24 +0900

    Add mode option to slack_diff

--- functions/slack_diff.js
+++ functions/slack_diff.js
@@ -1,13 +1,26 @@
 const jsdiff = require('diff');
 
-const slackDiff = (newStr, oldStr) => {
+const slackDiff = (newStr, oldStr, mode) => {
   if(typeof newStr === 'undefined') { throw new Error('invalid newStr'); }
 
   if(newStr === null) { newStr = ""; }
   if(typeof oldStr === 'undefined' || oldStr === null) { oldStr = ""; }
 
+  var diffs;
   var diffStr = '';
-  jsdiff.diffLines(oldStr, newStr).forEach( (d) => {
+  switch(mode) {
+    case 'chars':
+      diffs = jsdiff.diffChars(oldStr, newStr);
+      break;
+    case 'words':
+      diffs = jsdiff.diffWords(oldStr, newStr);
+      break;
+    case 'lines':
+    default:
+      diffs = jsdiff.diffLines(oldStr, newStr);
+      break;
+  }
+  diffs.forEach( (d) => {
     if(!d.value.match((/^\n+$/))) {
       if(d.added) {
         diffStr += `+ ${d.value.trim().replace(/\n/g, '\n  ')}\n`;
